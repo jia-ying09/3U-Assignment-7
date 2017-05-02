da5a143e95567192b6d5f40883fcd64685a172ff
linj4653 <linj4653@HRH-IC0027347.wrdsb.ca>
2017-05-02 10:13:49 -0400

    First Draft

---
+++ src/A7Q1.java
@@ -0,0 +1,45 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author linj4653
+ */
+public class A7Q1 {
+    /**
+     * 
+     * @param radius
+     * @return 
+     */
+    public static double circleArea(double radius) {
+           double rSquared = Math.pow(radius, 2);
+        double area = Math.PI*rSquared;
+            return area;
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // create new scanner
+        Scanner input = new Scanner (System.in);
+        
+        //ask user to enter the radius
+        System.out.println("Enter the radius of the circle in centimeters;");
+        
+        //input the radius of the circle
+        double radius = input.nextDouble();
+        
+        double answer = circleArea(radius);
+        
+        answer = Math.round(answer*100.0)/100.0;
+        
+        System.out.println("The area of the circle is " + answer + "cm squared.");
+        
+    }
+}
