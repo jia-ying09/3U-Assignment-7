a3cc113641c85deed40186603ef8c62537e42612
linj4653 <linj4653@HRH-IC0027347.wrdsb.ca>
2017-05-04 09:45:03 -0400

    First Draft

--- src/A7Q3.java
+++ src/A7Q3.java
@@ -5,44 +5,51 @@ import java.util.Scanner;
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author linj4653
  */
 public class A7Q3 {
-    
+
     /**
-     * 
-     * @param factor 
+     *
+     * @param factor
      */
-    public static void factors(int factor){
-        
-        factors(10);
-        
-        for (int i = 0; i < factor; i++){
-        factor = factor/ i;
-        
+    public static int factors(int factor) {
+        Scanner input = new Scanner (System.in);
+        for (int i = 10; i > 0; i++) {
         }
-      //  for (int i = 0; i < factor; i++){
-        System.out.println (factor + ", ");
-        
-    
+
+        int number = input.nextInt();
+        return 0;
+
     }
-  
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+
         // create scanner
-        Scanner input = new Scanner (System.in);
-        
+        Scanner input = new Scanner(System.in);
+
+        //ask for the number
+        System.out.println("Please enter the number you want factored:");
+
         //input the number the user entered
         int number = input.nextInt();
-        
-      //  factors(10);
-        
-        
+
+        //looping through all the numbers the number can be divided by
+        for (int i = 0; i == number; i++) {
+            int factor = number/i;
+            System.out.println(factor);
+        }
+
+
+        //divide the number by numbers that can be divided
+        //int factors = number / i;
+
+     //   System.out.print(factors);
     }
 }
+
