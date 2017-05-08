bacd40422d5dc2388287f48ecd76209598c5a412
linj4653 <linj4653@HRH-IC0027347.wrdsb.ca>
2017-05-08 10:15:38 -0400

    First Draft

--- src/A7Q3.java
+++ src/A7Q3.java
@@ -15,14 +15,8 @@ public class A7Q3 {
      *
      * @param factor
      */
-    public static int factors(int factor) {
-        Scanner input = new Scanner (System.in);
-        for (int i = 10; i > 0; i++) {
-        }
-
-        int number = input.nextInt();
-        return 0;
-
+    public static void factors(int numbers) {
+        System.out.println(numbers + ", ");
     }
 
     /**
@@ -37,19 +31,16 @@ public class A7Q3 {
         System.out.println("Please enter the number you want factored:");
 
         //input the number the user entered
-        int number = input.nextInt();
+        int numbers = input.nextInt();
 
-        //looping through all the numbers the number can be divided by
-        for (int i = 0; i == number; i++) {
-            int factor = number/i;
-            System.out.println(factor);
+        for (int i = numbers; i > 0; i--) {
+
+            if (numbers % i == 0) {
+
+                
+            }
+            factors(numbers / i);
+                System.out.print(factors + ", ");
         }
-
-
-        //divide the number by numbers that can be divided
-        //int factors = number / i;
-
-     //   System.out.print(factors);
     }
 }
-
