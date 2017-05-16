8224c78dd2f40af304e3f7fedcbd7baee61f07f6
linj4653 <linj4653@HRH-IC0027347.wrdsb.ca>
2017-05-16 09:19:07 -0400

    First Draft

--- src/A7Q9.java
+++ src/A7Q9.java
@@ -13,41 +13,29 @@ public class A7Q9 {
 
     public static void allDigitsOdd(int number) {
 
-        while (true) {
-            //see if the first digit is divisiable
+        //else statement
+        for (int i = 0; i < number / 10; i++) {
+
+            //pulling apart the number
+            number = number / 10;}
+
+            //see if the digits are divisiable
             if (number % 2 == 0) {
 
                 //output that there are no even numbers
                 System.out.println("There is an even number.");
-                break;
 
-                //else statement
             } else {
-
-                for (int i = 0; i < number / 10; i++) {
-
-                    //pulling apart the number
-                    number = number / 10;
-
-                    //see if the digits are divisiable
-                    if (number % 2 == 0) {
-
-                        //output that there are no even numbers
-                        System.out.println("There is an even number.");
-
-                        break;
-
-                    }
-                }
+                //output that there are no even numbers
+                System.out.println("There are no even numbers.");
             }
-        } //output that there are no even numbers
-        System.out.println("There are no even numbers.");
-    }
+        }
+    
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+/**
+ * @param args the command line arguments
+ */
+public static void main(String[] args) {
         // create a scanner
         Scanner input = new Scanner(System.in);
 
